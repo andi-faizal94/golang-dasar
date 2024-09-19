53e8295ceab9d571a0fdac229b3256e36f39bf1d
andi faizal <andifaizal@MacBook-Pro-andi.local>
2024-09-19 13:31:18 +0700

    belajar golang

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+
+func main(){
+
+	// var name string  ="andi"
+	// var age uint8 = 30
+	// var isMarried bool = true
+
+	// var fruits = [4]string{"apple", "grape", "banana", "melon"}
+
+// for i, fruit := range fruits {
+//     fmt.Printf("elemen %d : %s\n", i, fruit)
+// 	fmt.Println("element: ", i , fruit)
+// }
+
+var s  = [3]int{1,2,3}
+var a = [2]int{2,3}
+
+fmt.Println(len(s))
+fmt.Println(len(a))
+
+
+	// lastName := new(string)
+	// fmt.Println("Hello")
+	// fmt.Println("name :",name)
+	// fmt.Println("lastName :",lastName)
+	// fmt.Println("lastName :",*lastName)
+	// fmt.Println("age :",age)
+	// fmt.Println("isMarried :",isMarried)
+	// fmt.Println("Buah Kesukaan :",fruits)
+
+	// if isMarried == true{
+	// 	fmt.Println("Andi sudah menikah")
+	// }else {
+	// 	fmt.Println("Andi belum menikah")
+
+	// }
+
+}
